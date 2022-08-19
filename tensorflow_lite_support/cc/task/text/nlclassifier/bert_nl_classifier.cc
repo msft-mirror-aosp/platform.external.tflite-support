9fc1fcbd2239b6c8f6fa8399c6580d4f350e2150
Ben Nissan <bennissan@google.com>
2022-08-19 16:44:36 +0000

    Return StatusOr from Classify() methods
    This CL updates the Classify() methods in NLClassifier and
    BertNLClassifier to return StatusOr wrappers for their contents,
    allowing errors to be propagated across the JNI boundary.
    
    To enable this, it fixes a bug when checking for dynamic vs static input
    tensors in bert_nl_classifier.cc and throws exceptions from
    RunClassifier().
    
    Bug: 242926638, Bug: 242926783
    Test: atest tflite_support_classifier_tests
    Test: atest TfliteSupportClassifierTests
    Change-Id: Ib93385061ace97cf96a168e3d76d2d9162c8055f

--- tensorflow_lite_support/cc/task/text/nlclassifier/bert_nl_classifier.cc
+++ tensorflow_lite_support/cc/task/text/nlclassifier/bert_nl_classifier.cc
@@ -245,13 +245,17 @@ absl::Status BertNLClassifier::InitializeFromMetadata() {
                         segment_ids_tensor.dims->data[1]),
         TfLiteSupportStatus::kInvalidInputTensorSizeError);
   }
-  if (ids_tensor.dims_signature->data[1] == -1 &&
-      mask_tensor.dims_signature->data[1] == -1 &&
-      segment_ids_tensor.dims_signature->data[1] == -1) {
+
+  bool has_valid_dims_signature = ids_tensor.dims_signature->size == 2 &&
+                                  mask_tensor.dims_signature->size == 2 &&
+                                  segment_ids_tensor.dims_signature->size == 2;
+  int num_dynamic_tensors = ids_tensor.dims_signature->data[1] == -1 +
+                            mask_tensor.dims_signature->data[1] == -1 +
+                            segment_ids_tensor.dims_signature->data[1] == -1;
+
+  if (has_valid_dims_signature && num_dynamic_tensors == 3) {
     input_tensors_are_dynamic_ = true;
-  } else if (ids_tensor.dims_signature->data[1] == -1 ||
-             mask_tensor.dims_signature->data[1] == -1 ||
-             segment_ids_tensor.dims_signature->data[1] == -1) {
+  } else if (has_valid_dims_signature && num_dynamic_tensors > 0 && num_dynamic_tensors < 3) {
     return CreateStatusWithPayload(
         absl::StatusCode::kInternal,
         "Input tensors contain a mix of static and dynamic tensors",
